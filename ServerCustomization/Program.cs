fac5f2c9e7d9074de03db6f0c6b84e755da340de
remoad23 <patrick24p@gmail.com>
2026-02-28 14:54:31 +0100

    -

---
+++ ServerCustomization/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+// add here later stuff like Controller, Views.. and so on
+// basically all custom implementation that is not the internal framework
+// but a developers  high-level implementation
